7e6a4f6240cd4ef501bc84f5ffdd2435a8a2c91d
fktz94 <facundo.ortiz@live.com>
2023-10-09 15:54:34 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header>
-      <Link to="/">GoToHome</Link>
+      <Link to="/">Home</Link>
     </header>
   );
 }
